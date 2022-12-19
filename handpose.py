b0cc2ab49c279b78b642ee5077843c93fd77e281
Markus <gerhards@gendis.de>
2022-12-19 23:55:11 +0100

    wip

--- handpose.py
+++ handpose.py
@@ -1,8 +1,18 @@
+# https://www.analyticsvidhya.com/blog/2021/07/building-a-hand-tracking-system-using-opencv/
 import cv2
 import time
+import mediapipe as mp
 
 cap = cv2.VideoCapture(0)
 pTime = 0
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(static_image_mode=False,
+                      max_num_hands=2,
+                      min_detection_confidence=0.5,
+                      min_tracking_confidence=0.5)
+mpDraw = mp.solutions.drawing_utils
+    
 cv2.namedWindow("test")
 while True:
     success, img = cap.read()
@@ -10,8 +20,23 @@ while True:
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    #cv2.putText(img, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    cv2.imshow("Test", imgRGB)
+    results = hands.process(imgRGB)
+    
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                #print(id,lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x *w), int(lm.y*h)
+                #if id ==0:
+                cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
+
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+   
+    
+    cv2.putText(img, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.imshow("Test", img)
     
     k = cv2.waitKey(1)
     if k%256 == 27:
